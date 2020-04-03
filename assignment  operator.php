0682c7e2c75faf5b031c60108a99e08eed61f451
shivamvora <59314521+shivamvora@users.noreply.github.com>
2020-04-03 11:19:09 +0530

    Add files via upload

---
+++ assignment  operator.php
@@ -0,0 +1,31 @@
+<?php
+
+	$price=25.26;
+	echo $price;
+
+    echo "<br>";
+
+$ram=$shyam=$rohan=10;
+
+echo "Ram value's is: ",$ram;
+echo "<br>";
+echo "Shyam value's is: ",$shyam;
+echo "<br>";
+echo "rohan value's is: ",$rohan;
+echo "<br>";
+				
+				
+$m=15;
+					
+$m+=10;
+					
+echo $m;
+
+
+
+
+
+
+
+
+?>
